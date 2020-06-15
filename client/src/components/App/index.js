f1dc98ab3d503c51d603aec3a99c5d7c86a969ef
Stephen Jochen <stephenjochen2@gmail.com>
2020-06-14 20:37:23 -0400

    Initial file structure

---
+++ client/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React App
+      </header>
+    </div>
+  );
+}
+
+export default App;
