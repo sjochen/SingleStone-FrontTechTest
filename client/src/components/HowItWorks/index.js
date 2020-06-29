ef28449101e0baba65c4ffcfe1ddcc0753023868
Stephen Jochen <stephenjochen2@gmail.com>
2020-06-29 16:35:09 -0400

    populated the data from the json object to the screen

--- client/src/components/HowItWorks/index.js
+++ client/src/components/HowItWorks/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Steps from '../../steps.json';
 
 function howItWorks() {
-    return(
+    return (
         <div>
             <h1>How It Works</h1>
-            
+            {Steps.map(steps => {
+                return (
+                    <div>
+                        <h1>{steps.stepNumber}</h1>
+                        {steps.versionContent.map(name => {
+                            return (
+                                <div>
+                                    <h3>{name.title}</h3>
+                                    <p>{name.body}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+
+            })}
         </div>
     )
 };
