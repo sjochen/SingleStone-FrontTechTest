fb0435911f63d379ad5a84f57dd5b1e43835071a
Stephen Jochen <stephenjochen2@gmail.com>
2020-06-29 17:18:46 -0400

    added axios to call the api

--- client/src/components/HowItWorks/index.js
+++ client/src/components/HowItWorks/index.js
@@ -1,28 +1,39 @@
 import React from 'react';
-import Steps from '../../steps.json';
+import axios from 'axios';
+
 
 function howItWorks() {
-    return (
-        <div>
-            <h1>How It Works</h1>
-            {Steps.map(steps => {
-                return (
-                    <div>
-                        <h1>{steps.stepNumber}</h1>
-                        {steps.versionContent.map(name => {
-                            return (
-                                <div>
-                                    <h3>{name.title}</h3>
-                                    <p>{name.body}</p>
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
 
-            })}
-        </div>
-    )
+    axios(
+        {
+            method: "GET",
+            url: "https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge"
+        }
+
+    ).then(res => {
+        console.log(res.data)
+        return (
+            <div>
+                <h1>How It Works</h1>
+                {res.data.sort((a, b) => a.stepNumber - b.stepNumber).map(steps => {
+                    return (
+                        <div>
+                            <h1>{steps.stepNumber}</h1>
+                            {steps.versionContent.map(name => {
+                                return (
+                                    <div>
+                                        <h3>{name.title}</h3>
+                                        <p>{name.body}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+
+                })}
+            </div>
+        )
+    })
 };
 
 export default howItWorks;
