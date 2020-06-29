7daa850d2142cbb26a4ab04a13519270b7a4118d
Stephen Jochen <stephenjochen2@gmail.com>
2020-06-29 13:18:31 -0400

    finished out the text in the how it works section

--- client/src/components/HowItWorks/index.js
+++ client/src/components/HowItWorks/index.js
@@ -11,18 +11,18 @@ function howItWorks() {
             </div>
             <div>
                 <h1>02</h1>
-                <h2>FILL OUT A PROFILE</h2>
-                <p>We only want you to get games and gear that you'll love, so we'll ask you for preferences up front.</p>
+                <h2>REQUEST A DELIVERY</h2>
+                <p>Once you're ready for your first delivery, all it takes is a click to get your shipment on the way.</p>
             </div>
             <div>
                 <h1>03</h1>
-                <h2>FILL OUT A PROFILE</h2>
-                <p>We only want you to get games and gear that you'll love, so we'll ask you for preferences up front.</p>
+                <h2>KEEP WHAT YOU WANT</h2>
+                <p>Tell us "no thanks" by returning any unwanted products in the enclosed packaging.</p>
             </div>
             <div>
                 <h1>04</h1>
-                <h2>FILL OUT A PROFILE</h2>
-                <p>We only want you to get games and gear that you'll love, so we'll ask you for preferences up front.</p>
+                <h2>REQUEST ANOTHER DELIVERY</h2>
+                <p>Get your next gaming fix by updation your profile then initiating your next shipment.</p>
             </div>
         </div>
     )
