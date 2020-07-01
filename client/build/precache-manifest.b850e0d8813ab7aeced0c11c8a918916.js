59b2578e2c846e0140ffa282fba9aaaa3e72bb41
Stephen Jochen <stephenjochen2@gmail.com>
2020-06-30 22:29:51 -0400

    added to the package.json

---
+++ client/build/precache-manifest.b850e0d8813ab7aeced0c11c8a918916.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962e4fca599808b01d98d577eebde1d3",
+    "url": "/SingleStone-FrontTechTest/index.html"
+  },
+  {
+    "revision": "965afd113f8e223d6783",
+    "url": "/SingleStone-FrontTechTest/static/css/main.56c1598b.chunk.css"
+  },
+  {
+    "revision": "8b2e9eb62904ed6461f6",
+    "url": "/SingleStone-FrontTechTest/static/js/2.7c8c09a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SingleStone-FrontTechTest/static/js/2.7c8c09a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965afd113f8e223d6783",
+    "url": "/SingleStone-FrontTechTest/static/js/main.141bca8b.chunk.js"
+  },
+  {
+    "revision": "075427606e9af6fb946a",
+    "url": "/SingleStone-FrontTechTest/static/js/runtime-main.bea86407.js"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/SingleStone-FrontTechTest/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "40fc43b8112f83891a57a35f578254ad",
+    "url": "/SingleStone-FrontTechTest/static/media/logo-endless.40fc43b8.svg"
+  },
+  {
+    "revision": "ccd3c74c83d1df9cb84da4d9ea237c60",
+    "url": "/SingleStone-FrontTechTest/static/media/photo-couch.ccd3c74c.jpg"
+  }
+]);
